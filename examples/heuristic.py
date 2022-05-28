08dfd7a52652f086575ff1fcc83e3e58aad7d1cf
Oliver Overend <ollyoverend10@gmail.com>
2022-05-28 09:43:03 +0100

    refactor: add eval_heuristic_agent

--- examples/heuristic.py
+++ examples/heuristic.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+import gym
+
 sys.path.append(os.getcwd())
 
 import numpy as np
@@ -10,14 +12,12 @@ from gym_simplifiedtetris.agents import HeuristicAgent
 from gym_simplifiedtetris.envs import SimplifiedTetrisBinaryEnv as Tetris
 
 
-def main() -> None:
+def eval_heuristic_agent(
+    agent: HeuristicAgent, env: gym.Env, num_episodes: int
+) -> None:
     """Evaluate an agent that selects action according to a heuristic."""
-    num_episodes = 10
     episode_num = 0
     ep_returns = np.zeros(num_episodes)
-
-    agent = HeuristicAgent()
-    env = Tetris(grid_dims=(10, 10), piece_size=4)
 
     obs = env.reset()
 
@@ -38,6 +38,22 @@ def main() -> None:
 
     print(
         f"""\nScore obtained from averaging over {num_episodes} games:\nMean = {np.mean(ep_returns):.1f}\nStandard deviation = {np.std(ep_returns):.1f}"""
+    )
+
+
+def main() -> None:
+    """Evaluate an Heuristic agent."""
+    grid_dims = (10, 10)
+
+    agent = HeuristicAgent()
+    env = Tetris(grid_dims=grid_dims, piece_size=4)
+
+    num_episodes = 100
+
+    eval_heuristic_agent(
+        agent=agent,
+        env=env,
+        num_episodes=num_episodes,
     )
 
 
