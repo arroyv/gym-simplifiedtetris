f5c32913a790b613b5ae14dbb1eb5c840311b917
Oliver Overend <ollyoverend10@gmail.com>
2022-06-17 19:59:15 +0100

    fix: fix test_get_dellacherie_scores_empty_grid
    Change the array to compare in test_get_dellacherie_scores_empty_grid.

--- tests/test_heuristic_agent.py
+++ tests/test_heuristic_agent.py
@@ -20,57 +20,59 @@ class DellacherieAgentTest(unittest.TestCase):
         del self.env
         del self.agent
 
-    def test__get_dellacherie_scores_empty_grid(self) -> None:
+    def test_get_dellacherie_scores_empty_grid(self) -> None:
         self.env._engine._piece = Polymino(self.env.piece_size, 0)
-        array_to_compare = np.array(
-            [
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                614.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                4.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                312.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                302.0,
-            ]
+        # array_to_compare = np.array(
+        #     [
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         614.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         4.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         312.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         0.0,
+        #         302.0,
+        #     ]
+        # )
+        array_to_compare = np.NINF * np.ones(
+            self.env._engine._num_actions, dtype="double"
         )
-        array_to_compare = np.zeros(self.env._engine._num_actions)
-        array_to_compare[10] = abs(0 - 6) * 100 + 10 - (90 / 90) + 5
-        array_to_compare[16] = abs(6 - 6) * 100 + 0 - (90 / 90) + 5
-        array_to_compare[27] = abs(3 - 6) * 100 + 10 - (270 / 90) + 5
-        array_to_compare[33] = abs(9 - 6) * 100 + 0 - (270 / 90) + 5
+        array_to_compare[10] = abs(0 - 6) * 100 + 10 - (90 / 90)
+        array_to_compare[16] = abs(6 - 6) * 100 + 0 - (90 / 90)
+        array_to_compare[27] = abs(3 - 6) * 100 + 10 - (270 / 90)
+        array_to_compare[33] = abs(9 - 6) * 100 + 0 - (270 / 90)
         np.testing.assert_array_equal(
-            self.agent._get_dellacherie_scores(self.env),
+            self.agent._compute_dell_scores(self.env),
             array_to_compare,
         )
 
-    def test__get_dellacherie_funcs_populated_grid(self) -> None:
+    def test_get_dellacherie_funcs_populated_grid(self) -> None:
         self.env._engine._grid[:, -5:] = True
         self.env._engine._grid[
             1, self.env._engine._height - 5 : self.env._engine._height - 1
@@ -96,7 +98,7 @@ class DellacherieAgentTest(unittest.TestCase):
         self.env._engine._update_grid(True)
         self.env._engine._clear_rows()
         array_to_compare = np.array(
-            [func(self.env) for func in self.agent._get_dellacherie_funcs()]
+            [func(self.env) for func in self.agent._get_dell_funcs()]
         )
         np.testing.assert_array_equal(
             array_to_compare,
@@ -105,7 +107,7 @@ class DellacherieAgentTest(unittest.TestCase):
             ),
         )
 
-    def test__get_landing_height_I_piece_(self) -> None:
+    def test_get_landing_height_I_piece_(self) -> None:
         self.env._engine._piece = Polymino(self.env.piece_size, 0)
         self.env._engine._anchor = [0, self.env._engine._height - 1]
         self.env._engine._update_grid(True)
@@ -113,10 +115,10 @@ class DellacherieAgentTest(unittest.TestCase):
             self.agent._get_landing_height(self.env), 0.5 * (1 + self.env.piece_size)
         )
 
-    def test__get_eroded_cells_empty(self) -> None:
+    def test_get_eroded_cells_empty(self) -> None:
         self.assertEqual(self.agent._get_eroded_cells(self.env), 0)
 
-    def test__get_eroded_cells_single(self) -> None:
+    def test_get_eroded_cells_single(self) -> None:
         self.env._engine._grid[:, self.env._engine._height - 1 :] = True
         self.env._engine._grid[0, self.env._engine._height - 1] = False
         self.env._engine._piece = Polymino(self.env.piece_size, 0)
@@ -126,75 +128,75 @@ class DellacherieAgentTest(unittest.TestCase):
         self.env._engine._clear_rows()
         self.assertEqual(self.agent._get_eroded_cells(self.env), 1)
 
-    def test__get_row_transitions_empty(self) -> None:
+    def test_get_row_transitions_empty(self) -> None:
         self.assertEqual(self.agent._get_row_transitions(self.env), 40)
 
-    def test__get_row_transitions_populated(self) -> None:
+    def test_get_row_transitions_populated(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 1] = False
         self.env._engine._grid[2, self.env._engine._height - 1] = False
         self.env._engine._grid[1, self.env._engine._height - 2] = False
         self.assertEqual(self.agent._get_row_transitions(self.env), 42)
 
-    def test__get_row_transitions_populated_more_row_transitions(self) -> None:
+    def test_get_row_transitions_populated_more_row_transitions(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 2 :] = False
         self.env._engine._grid[2, self.env._engine._height - 2 :] = False
         self.env._engine._grid[4, self.env._engine._height - 1] = False
         np.testing.assert_array_equal(self.agent._get_row_transitions(self.env), 46)
 
-    def test__get_column_transitions_empty(self) -> None:
-        self.assertEqual(self.agent._get_column_transitions(self.env), 10)
+    def test_get_column_transitions_empty(self) -> None:
+        self.assertEqual(self.agent._get_col_transitions(self.env), 10)
 
-    def test__get_column_transitions_populated(self) -> None:
+    def test_get_column_transitions_populated(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 1] = False
         self.env._engine._grid[2, self.env._engine._height - 1] = False
         self.env._engine._grid[1, self.env._engine._height - 2] = False
-        self.assertEqual(self.agent._get_column_transitions(self.env), 14)
+        self.assertEqual(self.agent._get_col_transitions(self.env), 14)
 
-    def test__get_column_transitions_populated_less_column_transitions(self) -> None:
+    def test_get_column_transitions_populated_less_column_transitions(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 2 :] = False
         self.env._engine._grid[2, self.env._engine._height - 2 :] = False
         self.env._engine._grid[4, self.env._engine._height - 1] = False
-        np.testing.assert_array_equal(self.agent._get_column_transitions(self.env), 12)
+        np.testing.assert_array_equal(self.agent._get_col_transitions(self.env), 12)
 
-    def test__get_holes_empty(self) -> None:
+    def test_get_holes_empty(self) -> None:
         self.assertEqual(self.agent._get_holes(self.env), 0)
 
-    def test__get_holes_populated_two_holes(self) -> None:
+    def test_get_holes_populated_two_holes(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 1] = False
         self.env._engine._grid[2, self.env._engine._height - 1] = False
         self.assertEqual(self.agent._get_holes(self.env), 2)
 
-    def test__get_holes_populated_no_holes(self) -> None:
+    def test_get_holes_populated_no_holes(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 2 :] = False
         self.assertEqual(self.agent._get_holes(self.env), 0)
 
-    def test__get_holes_populated_one_hole(self) -> None:
+    def test_get_holes_populated_one_hole(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 2 :] = False
         self.env._engine._grid[2, self.env._engine._height - 2 :] = False
         self.env._engine._grid[4, self.env._engine._height - 1] = False
         np.testing.assert_array_equal(self.agent._get_holes(self.env), 1)
 
-    def test__get_cumulative_wells_empty(self) -> None:
-        np.testing.assert_array_equal(self.agent._get_cumulative_wells(self.env), 0)
+    def test_get_cumulative_wells_empty(self) -> None:
+        np.testing.assert_array_equal(self.agent._get_cum_wells(self.env), 0)
 
-    def test__get_cumulative_wells_populated(self) -> None:
+    def test_get_cumulative_wells_populated(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 2 :] = False
-        np.testing.assert_array_equal(self.agent._get_cumulative_wells(self.env), 3)
+        np.testing.assert_array_equal(self.agent._get_cum_wells(self.env), 3)
 
-    def test__get_cumulative_wells_populated_deeper_well(self) -> None:
+    def test_get_cumulative_wells_populated_deeper_well(self) -> None:
         self.env._engine._grid[:, -2:] = True
         self.env._engine._grid[0, self.env._engine._height - 2 :] = False
         self.env._engine._grid[2, self.env._engine._height - 2 :] = False
         self.env._engine._grid[4, self.env._engine._height - 1] = False
-        np.testing.assert_array_equal(self.agent._get_cumulative_wells(self.env), 6)
+        np.testing.assert_array_equal(self.agent._get_cum_wells(self.env), 6)
 
 
 if __name__ == "__main__":
--- tests/test_simplified_tetris_engine.py
+++ tests/test_simplified_tetris_engine.py
@@ -1,10 +1,161 @@
-"""Testing SimplifiedTetrisEngine."""
+"""Testing the class SimplifiedTetrisEngine."""
 
 import unittest
 
 import numpy as np
+import pytest
 from gym_simplifiedtetris.auxiliary import Polymino
 from gym_simplifiedtetris.envs import SimplifiedTetrisEngine as Engine
+
+
+@pytest.fixture(scope="session")
+def grid_dims():
+    return (20, 10)
+
+
+@pytest.fixture(scope="session")
+def piece_size():
+    return 4
+
+
+@pytest.fixture(scope="function")
+def engine(grid_dims, piece_size):
+    num_actions, num_pieces = {
+        1: (grid_dims[1], 1),
+        2: (2 * grid_dims[1] - 1, 1),
+        3: (4 * grid_dims[1] - 4, 2),
+        4: (4 * grid_dims[1] - 6, 7),
+    }[piece_size]
+
+    engine = Engine(
+        grid_dims=grid_dims,
+        piece_size=piece_size,
+        num_pieces=num_pieces,
+        num_actions=num_actions,
+    )
+
+    engine._reset()
+
+    return engine
+
+
+def test_is_illegal_non_empty_overlapping(engine, piece_size):
+    engine._piece = Polymino(piece_size, 0)
+    engine._anchor = [0, engine._height - 1]
+    engine._grid[0, engine._height - 1] = 1
+
+    assert engine._is_illegal()
+
+
+def test_hard_drop_empty_grid(engine, piece_size) -> None:
+    engine._piece = Polymino(piece_size, 0)
+    engine._anchor = [0, 0]
+    engine._hard_drop()
+
+    assert engine._anchor, [0, engine._height - 1]
+
+
+def test_hard_drop_non_empty_grid(engine, piece_size) -> None:
+    engine._piece = Polymino(piece_size, 0)
+    engine._anchor = [0, 0]
+    engine._grid[0, engine._height - 1] = 1
+    engine._hard_drop()
+
+    assert engine._anchor == [0, engine._height - 2]
+
+
+def test_clear_rows_output_with_empty_grid(engine, piece_size) -> None:
+    assert engine._clear_rows() == 0
+
+
+def test_clear_rows_empty_grid_after(engine, piece_size) -> None:
+    engine._clear_rows()
+    grid_after = np.zeros((engine._width, engine._height), dtype="bool")
+
+    np.testing.assert_array_equal(engine._grid, grid_after)
+
+
+def test_clear_rows_output_one_full_row(engine, piece_size) -> None:
+    engine._grid[:, engine._height - 1 :] = 1
+
+    assert engine._clear_rows() == 1
+
+
+def test_clear_rows_one_full_row_grid_after(engine, piece_size) -> None:
+    engine._grid[:, engine._height - 1 :] = 1
+    engine._clear_rows()
+    grid_after = np.zeros((engine._width, engine._height), dtype="bool")
+
+    np.testing.assert_array_equal(engine._grid, grid_after)
+
+
+def test_clear_rows_output_two_full_rows(engine, piece_size) -> None:
+    engine._grid[:, engine._height - 2 :] = 1
+
+    assert engine._clear_rows() == 2
+
+
+def test_clear_rows_two_full_rows_grid_after(engine, piece_size) -> None:
+    engine._grid[:, engine._height - 2 :] = 1
+    engine._clear_rows()
+    grid_after = np.zeros((engine._width, engine._height), dtype="bool")
+
+    np.testing.assert_array_equal(engine._grid, grid_after)
+
+
+def test_clear_rows_output_two_full_rows_full_cell_above(engine, piece_size) -> None:
+    engine._grid[:, engine._height - 2 :] = 1
+    engine._grid[3, engine._height - 3] = 1
+
+    assert engine._clear_rows() == 2
+
+
+def test_clear_rows_two_full_rows_full_cell_above_grid_after(
+    engine, piece_size
+) -> None:
+    engine._grid[:, engine._height - 2 :] = 1
+    engine._grid[3, engine._height - 3] = 1
+    engine._clear_rows()
+    grid_after = np.zeros((engine._width, engine._height), dtype="bool")
+    grid_after[3, engine._height - 1] = 1
+
+    np.testing.assert_array_equal(engine._grid, grid_after)
+
+
+def test_update_grid_simple(engine, piece_size) -> None:
+    engine._piece = Polymino(piece_size, 0)
+    engine._anchor = [0, engine._height - 1]
+    grid_to_compare = np.zeros((engine._width, engine._height), dtype="bool")
+    grid_to_compare[0, engine._height - piece_size :] = 1
+    engine._update_grid(True)
+
+    np.testing.assert_array_equal(engine._grid, grid_to_compare)
+
+
+def test_update_grid_empty(engine, piece_size) -> None:
+    engine._piece = Polymino(piece_size, 0)
+    engine._anchor = [0, engine._height - 1]
+    grid_to_compare = np.zeros((engine._width, engine._height), dtype="bool")
+    engine._update_grid(False)
+
+    np.testing.assert_array_equal(engine._grid, grid_to_compare)
+
+
+def test_update_grid_populated(engine, piece_size) -> None:
+    engine._piece = Polymino(piece_size, 0)
+    engine._grid[0, engine._height - piece_size :] = 1
+    engine._anchor = [0, engine._height - 1]
+    grid_to_compare = np.zeros((engine._width, engine._height), dtype="bool")
+    engine._update_grid(False)
+
+    np.testing.assert_array_equal(engine._grid, grid_to_compare)
+
+
+def test_compute_all_available_actions(engine, piece_size) -> None:
+    engine._compute_all_available_actions()
+
+    for value in engine._all_available_actions.values():
+        assert engine._num_actions == len(value)
 
 
 class SimplifiedTetrisEngineStandardTetrisTest(unittest.TestCase):
@@ -33,67 +184,78 @@ class SimplifiedTetrisEngineStandardTetrisTest(unittest.TestCase):
         self.engine._close()
         del self.engine
 
-    def test__is_illegal_non_empty_overlapping(self) -> None:
+    def test_is_illegal_non_empty_overlapping(self) -> None:
         self.engine._piece = Polymino(self.piece_size, 0)
         self.engine._anchor = [0, self.engine._height - 1]
         self.engine._grid[0, self.engine._height - 1] = 1
+
         self.assertEqual(self.engine._is_illegal(), True)
 
-    def test__hard_drop_empty_grid(self) -> None:
+    def test_hard_drop_empty_grid(self) -> None:
         self.engine._piece = Polymino(self.piece_size, 0)
         self.engine._anchor = [0, 0]
         self.engine._hard_drop()
+
         self.assertEqual(self.engine._anchor, [0, self.engine._height - 1])
 
-    def test__hard_drop_non_empty_grid(self) -> None:
+    def test_hard_drop_non_empty_grid(self) -> None:
         self.engine._piece = Polymino(self.piece_size, 0)
         self.engine._anchor = [0, 0]
         self.engine._grid[0, self.engine._height - 1] = 1
         self.engine._hard_drop()
+
         self.assertEqual(self.engine._anchor, [0, self.engine._height - 2])
 
-    def test__clear_rows_output_with_empty_grid(self) -> None:
+    def test_clear_rows_output_with_empty_grid(self) -> None:
+
         self.assertEqual(self.engine._clear_rows(), 0)
 
-    def test__clear_rows_empty_grid_after(self) -> None:
+    def test_clear_rows_empty_grid_after(self) -> None:
         self.engine._clear_rows()
         grid_after = np.zeros((self.engine._width, self.engine._height), dtype="bool")
+
         np.testing.assert_array_equal(self.engine._grid, grid_after)
 
-    def test__clear_rows_output_one_full_row(self) -> None:
+    def test_clear_rows_output_one_full_row(self) -> None:
         self.engine._grid[:, self.engine._height - 1 :] = 1
+
         self.assertEqual(self.engine._clear_rows(), 1)
 
-    def test__clear_rows_one_full_row_grid_after(self) -> None:
+    def test_clear_rows_one_full_row_grid_after(self) -> None:
         self.engine._grid[:, self.engine._height - 1 :] = 1
         self.engine._clear_rows()
         grid_after = np.zeros((self.engine._width, self.engine._height), dtype="bool")
+
         np.testing.assert_array_equal(self.engine._grid, grid_after)
 
-    def test__clear_rows_output_two_full_rows(self) -> None:
+    def test_clear_rows_output_two_full_rows(self) -> None:
         self.engine._grid[:, self.engine._height - 2 :] = 1
+
         self.assertEqual(self.engine._clear_rows(), 2)
 
-    def test__clear_rows_two_full_rows_grid_after(self) -> None:
+    def test_clear_rows_two_full_rows_grid_after(self) -> None:
         self.engine._grid[:, self.engine._height - 2 :] = 1
         self.engine._clear_rows()
         grid_after = np.zeros((self.engine._width, self.engine._height), dtype="bool")
+
         np.testing.assert_array_equal(self.engine._grid, grid_after)
 
-    def test__clear_rows_output_two_full_rows_full_cell_above(self) -> None:
+    def test_clear_rows_output_two_full_rows_full_cell_above(self) -> None:
         self.engine._grid[:, self.engine._height - 2 :] = 1
         self.engine._grid[3, self.engine._height - 3] = 1
+
         self.assertEqual(self.engine._clear_rows(), 2)
 
-    def test__clear_rows_two_full_rows_full_cell_above_grid_after(self) -> None:
+    def test_clear_rows_two_full_rows_full_cell_above_grid_after(self) -> None:
         self.engine._grid[:, self.engine._height - 2 :] = 1
         self.engine._grid[3, self.engine._height - 3] = 1
         self.engine._clear_rows()
         grid_after = np.zeros((self.engine._width, self.engine._height), dtype="bool")
         grid_after[3, self.engine._height - 1] = 1
+
         np.testing.assert_array_equal(self.engine._grid, grid_after)
 
-    def test__update_grid_simple(self) -> None:
+    def test_update_grid_simple(self) -> None:
         self.engine._piece = Polymino(self.piece_size, 0)
         self.engine._anchor = [0, self.engine._height - 1]
         grid_to_compare = np.zeros(
@@ -101,18 +263,20 @@ class SimplifiedTetrisEngineStandardTetrisTest(unittest.TestCase):
         )
         grid_to_compare[0, self.engine._height - self.piece_size :] = 1
         self.engine._update_grid(True)
+
         np.testing.assert_array_equal(self.engine._grid, grid_to_compare)
 
-    def test__update_grid_empty(self) -> None:
+    def test_update_grid_empty(self) -> None:
         self.engine._piece = Polymino(self.piece_size, 0)
         self.engine._anchor = [0, self.engine._height - 1]
         grid_to_compare = np.zeros(
             (self.engine._width, self.engine._height), dtype="bool"
         )
         self.engine._update_grid(False)
+
         np.testing.assert_array_equal(self.engine._grid, grid_to_compare)
 
-    def test__update_grid_populated(self) -> None:
+    def test_update_grid_populated(self) -> None:
         self.engine._piece = Polymino(self.piece_size, 0)
         self.engine._grid[0, self.engine._height - self.piece_size :] = 1
         self.engine._anchor = [0, self.engine._height - 1]
@@ -120,10 +284,12 @@ class SimplifiedTetrisEngineStandardTetrisTest(unittest.TestCase):
             (self.engine._width, self.engine._height), dtype="bool"
         )
         self.engine._update_grid(False)
+
         np.testing.assert_array_equal(self.engine._grid, grid_to_compare)
 
-    def test__compute_all_available_actions(self) -> None:
+    def test_compute_all_available_actions(self) -> None:
         self.engine._compute_all_available_actions()
+
         for value in self.engine._all_available_actions.values():
             self.assertEqual(self.engine._num_actions, len(value))
 
