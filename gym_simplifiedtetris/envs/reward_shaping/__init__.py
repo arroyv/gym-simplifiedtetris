73ace64b3d7a3284c9bbaf78e8b148c28a2f6033
Oliver Overend <ollyoverend10@gmail.com>
2022-06-23 07:00:55 +0100

    feat: add a reward shaping env based on the col heights

--- gym_simplifiedtetris/envs/reward_shaping/__init__.py
+++ gym_simplifiedtetris/envs/reward_shaping/__init__.py
@@ -1,6 +1,7 @@
 """Initialise reward_shaping/."""
 
 from .simplified_tetris_binary_shaped_env import SimplifiedTetrisBinaryShapedEnv
+from .simplified_tetris_heights_shaped_env import SimplifiedTetrisHeightsShapedEnv
 from .simplified_tetris_part_binary_shaped_env import (
     SimplifiedTetrisPartBinaryShapedEnv,
 )
@@ -8,4 +9,5 @@ from .simplified_tetris_part_binary_shaped_env import (
 __all__ = [
     "SimplifiedTetrisBinaryShapedEnv",
     "SimplifiedTetrisPartBinaryShapedEnv",
+    "SimplifiedTetrisHeightsShapedEnv",
 ]
