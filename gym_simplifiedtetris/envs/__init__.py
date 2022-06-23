68361396a073f51d6f847217894ccf2064f1b026
Oliver Overend <ollyoverend10@gmail.com>
2022-06-23 07:00:33 +0100

    feat: add a env that uses the col heights

--- gym_simplifiedtetris/envs/__init__.py
+++ gym_simplifiedtetris/envs/__init__.py
@@ -4,15 +4,15 @@ from gym_simplifiedtetris.envs._simplified_tetris_base_env import (
     _SimplifiedTetrisBaseEnv,
 )
 from gym_simplifiedtetris.envs._simplified_tetris_engine import SimplifiedTetrisEngine
-from gym_simplifiedtetris.envs.reward_shaping import (
-    SimplifiedTetrisBinaryShapedEnv,
-    SimplifiedTetrisPartBinaryShapedEnv,
-)
+from gym_simplifiedtetris.envs.reward_shaping import *
 from gym_simplifiedtetris.envs.simplified_tetris_binary_env import (
     SimplifiedTetrisBinaryEnv,
 )
 from gym_simplifiedtetris.envs.simplified_tetris_part_binary_env import (
     SimplifiedTetrisPartBinaryEnv,
+)
+from gym_simplifiedtetris.envs.simplified_tetris_heights_env import (
+    SimplifiedTetrisHeightsEnv,
 )
 
 __all__ = [
@@ -21,4 +21,6 @@ __all__ = [
     "SimplifiedTetrisBinaryShapedEnv",
     "SimplifiedTetrisPartBinaryEnv",
     "SimplifiedTetrisPartBinaryShapedEnv",
+    "SimplifiedTetrisHeightsEnv",
+    "SimplifiedTetrisHeightsShapedEnv",
 ]
